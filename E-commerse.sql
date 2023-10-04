593ecf1980f5ebcf23a73e0ab8aabdee5d22d6ef
OkarskiyBogdan <146741832+OkarskiyBogdan@users.noreply.github.com>
2023-10-04 11:41:55 +0300

    Add files via upload

--- E-commerse.sql
+++ E-commerse.sql
@@ -1,7 +1,16 @@
 ALTER TABLE ecommerce_customer_data_large
 DROP COLUMN `Customer Age`;
 
+   -- droping duplicate column to get more clear data
+   
+SELECT *
+FROM ecommerce_customer_data_large;
 
+-- get clean table to work with
+
+With CusFinInfo(`Customer ID`,TotalTransaction,TotalUnit,TotalPrice,AverageOrderValue,MinProfitMargin,PurchaseFruequency,CustomerValue) 
+as 
+(
 SELECT 
     `Customer ID`,
     COUNT(`Customer Name`) AS TotalTransaction,
@@ -22,8 +31,17 @@ FROM
 WHERE
     Returns != 1
 GROUP BY `Customer ID`
-ORDER BY TotalPrice DESC
+)
+SELECT *, CASE WHEN CustomerValue > 1 THEN 'S'
+			    WHEN CustomerValue > 0.8 THEN 'A' 
+                WHEN CustomerValue > 0.6 THEN 'B'
+                WHEN CustomerValue > 0.4 THEN 'C'
+                WHEN CustomerValue > 0.2 THEN 'D'
+                ELSE 'F'
+                END as Segmentation 
+FROM CusFinInfo;
 
+-- Get info about customers value and Segmentation customers
   
 SELECT 
     `Product Category`,
@@ -42,16 +60,25 @@ SELECT
     AVG(Age) AS AvgAGE
 FROM
     ecommerce_customer_data_large
-GROUP BY `Product Category`
+GROUP BY `Product Category`;
 
+-- Get persentage of gender and average age for each product category
 
-SELECT YEAR(`Purchase Date`),COUNT(`Customer Name`) as TotalOrders,SUM(`Quantity`) AS TotalUnitSold,SUM(`Total Purchase Amount`) AS TotalPriceByYear,SUM(`Total Purchase Amount`)*0.05 AS ProfitByYear
+SELECT YEAR(`Purchase Date`) as 'Year',COUNT(`Customer Name`) as TotalOrders,SUM(`Quantity`) AS TotalUnitSold,SUM(`Total Purchase Amount`) AS TotalPriceByYear,SUM(`Total Purchase Amount`)*0.05 AS ProfitByYear
 FROM ecommerce_customer_data_large
 WHERE
     Returns != 1
 GROUP BY YEAR(`Purchase Date`)
-ORDER BY YEAR(`Purchase Date`)
+ORDER BY YEAR(`Purchase Date`);
 
+-- Get statistics of e-commerce by years
 
-SELECT *
+SELECT DATE_FORMAT(`Purchase Date`, '%Y-%m') as YearAndMonth,SUM(CASE WHEN Returns = 1 THEN 1 ELSE 0 END) as Returns,Count(Returns) as Purchase, SUM(CASE WHEN Returns = 1 THEN 1 ELSE 0 END)/Count(Returns)*100 as Percent 
 FROM ecommerce_customer_data_large
+GROUP BY DATE_FORMAT(`Purchase Date`, '%Y-%m')
+ORDER BY DATE_FORMAT(`Purchase Date`, '%Y-%m');
+
+-- Get statistics of Returns by Month
+
+
+
